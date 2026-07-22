a7c8e9fd75a0e356bd6957adbf54d60130ce1749
margarita-123-b <marrano@ofimundo.cl>
2026-07-22 15:52:47 -0400

    fyi

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "d3d57fbyf4vdnc.cloudfront.net",
       },
       {
+        protocol: "http",
+        hostname: "d3d57fbyf4vdnc.cloudfront.net",
+      },
+      {
         protocol: "https",
+        hostname: "*.cloudfront.net",
+      },
+      {
+        protocol: "http",
         hostname: "*.cloudfront.net",
       },
     ],
